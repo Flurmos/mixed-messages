ebb2a15136eed135f334a2078db1e9f04fe97f36
Ryan <ryan@dittomail.com>
2025-01-29 19:13:56 -0500

    Add variables to hold the random indexes, as well as comments to explain the code

--- mixed-messages.js
+++ mixed-messages.js
@@ -2,7 +2,11 @@
 const frontEnd = ['We should', 'You should', 'I\'m going to', 'We\'re going to', 'I ought to', 'We ought to', 'Let\'s'];
 const activity = ['hiking', 'for a hike', 'swimming', 'biking', 'running', 'out', 'outside', 'touch grass', 'skydiving', 'rock climbing', 'fishing', 'kyaking', 'to the arcade', 'for a walk', 'skating', 'to the park'];
 const backEnd = ['today', 'tomorrow', 'soon', 'more often', 'regularly', 'more regularly', 'every day', 'every week'];
-
+// Generate a random index
 const randIndex = arr => Math.floor(Math.random() * arr.length);
-
-console.log(frontEnd[randIndex] + ' ' + activity[randIndex] + ' ' + backEnd[randIndex] + '!');
+// Get random index for each array
+const frontIdx = randIndex(frontEnd);
+const activityIdx = randIndex(activity);
+const backIdx = randIndex(backEnd);
+// Print the random message
+console.log(frontEnd[frontIdx] + ' go ' + activity[activityIdx] + ' ' + backEnd[backIdx] + '!');
